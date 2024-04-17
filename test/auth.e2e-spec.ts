306536c08bf909703b25390a3122488c3eda2478
Ashley Dodson <einlanzerous@live.com>
2024-04-17 04:21:28 +0000

    Fix local test running with sqlite and add another auth test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -28,4 +28,20 @@ describe('Auth Service (e2e)', () => {
         expect(email).toEqual(email);
       });
   });
+
+  test('Signup as a new user, then get the currently logged in user', async () => {
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
